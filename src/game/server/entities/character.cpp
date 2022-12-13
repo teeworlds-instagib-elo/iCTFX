c392fa5045fbe810b3da121bfedd3b59c6ac3c4e
JSaurusRex <jessetonckens@gmail.com>
2022-12-13 20:44:55 +0100

    sound fix

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -966,16 +966,16 @@ bool CCharacter::TakeDamage(vec2 Force, int Dmg, int From, int Weapon)
 	m_DamageTakenTick = Server()->Tick();
 
 	// do damage Hit sound
-	if(From >= 0 && From != m_pPlayer->GetCID() && GameServer()->m_apPlayers[From])
-	{
-		int64_t Mask = CmaskOne(From);
-		for(int i = 0; i < MAX_CLIENTS; i++)
-		{
-			if(GameServer()->m_apPlayers[i] && GameServer()->m_apPlayers[i]->GetTeam() == TEAM_SPECTATORS && GameServer()->m_apPlayers[i]->m_SpectatorID == From)
-				Mask |= CmaskOne(i);
-		}
-		GameServer()->CreateSound(GameServer()->m_apPlayers[From]->m_ViewPos, SOUND_HIT, Mask);
-	}
+	// if(From >= 0 && From != m_pPlayer->GetCID() && GameServer()->m_apPlayers[From])
+	// {
+	// 	int64_t Mask = CmaskOne(From);
+	// 	for(int i = 0; i < MAX_CLIENTS; i++)
+	// 	{
+	// 		if(GameServer()->m_apPlayers[i] && GameServer()->m_apPlayers[i]->GetTeam() == TEAM_SPECTATORS && GameServer()->m_apPlayers[i]->m_SpectatorID == From)
+	// 			Mask |= CmaskOne(i);
+	// 	}
+	// 	GameServer()->CreateSound(GameServer()->m_apPlayers[From]->m_ViewPos, SOUND_HIT, Mask);
+	// }
 
 	// check for death
 	if(m_Health <= 0)
