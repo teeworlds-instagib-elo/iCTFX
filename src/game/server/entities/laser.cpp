53a9d6f35978d5e0aef3d71e59ff0d4957817419
JSaurusRex <jessetonckens@gmail.com>
2025-10-18 12:14:27 +0200

    fix bots being able to kill teammate

--- src/game/server/entities/laser.cpp
+++ src/game/server/entities/laser.cpp
@@ -202,6 +202,10 @@ bool CLaser::HitCharacter(vec2 From, vec2 To)
 	m_Pos = At;
 	m_Energy = -1;
 	m_DidHit = true;
+
+	if(m_pBot && m_Team == pHit->m_pPlayer->GetTeam())
+		return true;
+	
 	pHit->TakeDamage(vec2(0.f, 0.f), GameServer()->Tuning()->m_LaserDamage, m_Owner, WEAPON_LASER, m_StartTick);
 	return true;
 }
